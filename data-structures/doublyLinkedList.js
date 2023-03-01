d0cfba4983a7874b7d4fec055d3bb70b8f0168f1
Adu Lawrence <aduaondohemba@gmail.com>
2023-03-01 23:48:48 +0100

    insert doubly linked list

--- data-structures/doublyLinkedList.js
+++ data-structures/doublyLinkedList.js
@@ -6,3 +6,27 @@ class Node
         this.previous = null;
     }
 }
+
+class DoublyLinkedList
+{
+    constructor()
+    {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    insert(node)
+    {
+        if(!this.head){
+            this.head = node;
+            this.tail = this.head;
+        }else{
+            node.previous = this.tail;
+            this.tail.next = node;
+            this.tail = this.tail.next;
+        }
+
+        return ++this.length;
+    }
+}
